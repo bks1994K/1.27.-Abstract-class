933a62fbf42966d15aa1e1b5d4366673cfc246c5
bks1994K <bks1994@mail.ru>
2022-10-20 16:39:06 +0300

    Add project files.

---
+++ Animals/FishAnimal.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Animals
+{
+    public class FishAnimal : AbstractAnimal
+    {
+        public FishAnimal(string name)
+        {
+            Type = "fish";
+            Name = name;
+            Habitat = "water";
+            HabitatArea = 50000;
+            Food = new[] { "mollusk", "seaweed" };
+            IsPredator = true;
+            Sound = "BulBul";
+            WeightFood = 2.5;
+        }
+        public override void Play()
+        {
+            Console.WriteLine($"{Name} hard to play underwater");
+        }
+        public void Swim()
+        {
+            Console.WriteLine($"{Name}: can breathe underwater");
+        }
+    }
+}
+
---
+++ Animals/Program.cs
@@ -0,0 +1,28 @@
+﻿using Animals;
+
+BirdAnimal cormorant = new BirdAnimal("Baklan");
+cormorant.Eat(new[] { "worm" }, new[] { 2.9 });
+cormorant.Play();
+cormorant.Fly();
+Console.WriteLine();
+
+FishAnimal[] fishes = new FishAnimal[]
+    {
+    new FishAnimal ("Nemo"),
+    new FishAnimal ("Dori"),
+    };
+for (int i = 0; i < fishes.Length; i++)
+{
+    fishes[i].Eat(new[] { "mollusk" }, new[] { 0.5 });
+    fishes[i].Play();
+    fishes[i].Swim();
+    fishes[i].Eat(new[] { "seaweed" }, new[] { 5.5 });
+}
+Console.WriteLine();
+
+PrimateAnimal caesar = new PrimateAnimal("Caesar");
+caesar.Eat(new[] { "bananas" }, new[] { 100d });
+caesar.Play();
+caesar.Climb();
+
+
